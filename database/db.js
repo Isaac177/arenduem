38ada707426f8c39f52c3a4e092c64556159d5d7
Isaac177 <isaacjogues177@gmail.com>
2023-07-29 10:18:54 +0200

    Fixing vulnerabilities

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
